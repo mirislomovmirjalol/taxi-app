8db82ae50703fce57debdaab1f10baba559552e9
mirjalol <mirislomovmirjalol@gmail.com>
2023-01-11 19:58:17 +0000

    bug fixes and design update

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -4,7 +4,8 @@ import tw from "tailwind-react-native-classnames";
 import Map from "./Map";
 import {Icon} from "@rneui/base";
 import {useNavigation} from "@react-navigation/native";
-import NavigateCard from "./NavigateCard";
+import {useSelector} from "react-redux";
+import {selectTravelTimeInformation} from "../slices/navSlice";
 
 const data = [
     {
@@ -29,6 +30,8 @@ const data = [
 const RideOptionsCard = () => {
     const navigation = useNavigation();
     const [selected, setSelected] = React.useState(null);
+    const travelTimeInformation = useSelector(selectTravelTimeInformation);
+
     return (
         <>
             <SafeAreaView style={tw`h-full bg-white flex-grow`}>
@@ -39,11 +42,11 @@ const RideOptionsCard = () => {
                 </View>
 
                 <TouchableOpacity
-                    onPress={() => navigation.navigate(NavigateCard)}
-                    style={tw`absolute top-14 left-5 rounded-full`}
+                    onPress={() => navigation.navigate("NavigateCard")}
+                    style={tw`absolute top-16 left-8 z-50 rounded-full shadow-lg`}
                 >
                     <Icon
-                        style={tw`p-2 bg-gray-800 rounded-full w-10 mt-4`}
+                        style={tw`p-2 bg-black rounded-full w-10 mt-4`}
                         name="arrowleft"
                         color="white"
                         type="antdesign"
@@ -53,30 +56,57 @@ const RideOptionsCard = () => {
                 <Text
                     style={tw`text-center py-5 text-xl border-t border-gray-200`}
                 >
-                    Pick a ride
+                    Pick a ride - {travelTimeInformation?.distance?.text}
                 </Text>
 
                 <FlatList
                     data={data}
                     keyExtractor={(item) => item.id}
-                    renderItem={({item: {id, title, multiplier, image}}) => (
-                        <TouchableOpacity style={tw`flex-row justify-between items-center px-10`}>
-                            <Image
+                    renderItem={({item: {id, title, multiplier, image}, item}) => (
+                        <TouchableOpacity
+                            onPress={() => setSelected(item)}
+                            style={tw`flex-row justify-between h-20 items-center px-5 mx-5 rounded-xl border-2 border-white ${
+                                id === selected?.id && "border-black bg-gray-100"
+                            }`}
+                        >
+                            {/*
+                                <Image
                                 style={{
-                                    width: 100,
-                                    height: 100,
-                                    resizeMode: "contain"
-                                }}
-                                source={{uri: image}}
+                                      width: 100,
+                                      height: 100,
+                                      resizeMode: "contain"
+                                    }}
+                                    source={{uri: image}}
+                                />
+                            */}
+
+                            <Icon
+                                name={"car"}
+                                type="ionicon"
+                                color="black"
+                                size={34}
                             />
-                            <View style={tw`-ml-6`}>
-                                <Text style={tw`text-xl font-semibold`}>{title}</Text>
-                                <Text>Travel time...</Text>
+                            <View style={tw`-ml-6 text-left`}>
+                                <Text style={tw`text-lg font-semibold`}>{title}</Text>
+                                <Text style={tw`text-xs font-light text-gray-600`}>{travelTimeInformation?.duration?.text + " " + 'travel time'}</Text>
                             </View>
-                            <Text style={tw`text-xl`}>$99</Text>
+                            <Text>
+                                {new Intl.NumberFormat('en-gb', {
+                                    style: 'currency',
+                                    currency: 'GBP'
+                                }).format(
+                                    (travelTimeInformation?.duration.value * 1.8) * multiplier / 100 + (multiplier * 2)
+                                )}
+                            </Text>
                         </TouchableOpacity>
                     )}
                 />
+
+                <View>
+                    <TouchableOpacity style={tw`bg-black py-3 m-3 rounded-lg`}>
+                        <Text style={tw`text-center text-white text-xl`}>Choose {selected?.title}</Text>
+                    </TouchableOpacity>
+                </View>
             </SafeAreaView>
         </>
     );
--- screens/RideScreen.js
+++ screens/RideScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {StyleSheet, Text, View} from "react-native";
+import {StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import Map from "../components/Map";
 import tw from "tailwind-react-native-classnames";
 import {createStackNavigator} from "@react-navigation/stack";
@@ -9,11 +9,14 @@ import RideOptionsCard from "../components/RideOptionsCard";
 import {useSelector} from "react-redux";
 import {selectDestination, selectOrigin} from "../slices/navSlice";
 import FavouritePlaces from "../components/FavouritePlaces";
+import {Icon} from "@rneui/base";
+import {useNavigation} from "@react-navigation/native";
 
 const RideScreen = () => {
     const origin = useSelector(selectOrigin);
     const destination = useSelector(selectDestination);
     const Stack = createStackNavigator();
+    const navigation = useNavigation();
 
     return (
         <View>
