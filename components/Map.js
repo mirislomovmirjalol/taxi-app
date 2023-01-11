dfaef19b6af263de24fe60d71c9dbfabcd894026
mirjalol <mirislomovmirjalol@gmail.com>
2023-01-11 19:57:40 +0000

    changes on map

--- components/Map.js
+++ components/Map.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useRef} from "react";
 import MapView, {Marker} from "react-native-maps";
-import {useSelector} from "react-redux";
-import {selectDestination, selectOrigin} from "../slices/navSlice";
+import {useDispatch, useSelector} from "react-redux";
+import {selectDestination, selectOrigin, setTravelTimeInformation} from "../slices/navSlice";
 import MapViewDirections from "react-native-maps-directions";
 import {GOOGLE_MAPS_APIKEY} from "@env";
 
@@ -9,6 +9,7 @@ const map = () => {
     const origin = useSelector(selectOrigin);
     const destination = useSelector(selectDestination);
     const mapRef = useRef(null);
+    const dispatch = useDispatch();
 
     useEffect(() => {
         if (!origin || !destination) return;
@@ -16,6 +17,20 @@ const map = () => {
             edgePadding: {top: 50, right: 50, bottom: 50, left: 50},
         });
     }, [origin, destination]);
+
+    useEffect(() => {
+        if (!origin || !destination) return;
+
+        const getTravelTime = async () => {
+            fetch(`https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${origin.description}&destinations=${destination.description}&key=${GOOGLE_MAPS_APIKEY}`)
+                .then((response) => response.json())
+                .then((data) => {
+                    dispatch(setTravelTimeInformation(data.rows[0].elements[0]));
+                });
+        }
+
+        getTravelTime();
+    }, [origin, destination, GOOGLE_MAPS_APIKEY]);
 
     return (
         <MapView
